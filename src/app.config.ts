3e2d33d75f5cff707ddfd13f19c21e02e8d00970
jiangchuangshi <jiangchuangshi@doba.com>
2024-11-13 15:50:34 +0800

    需求描述

--- src/app.config.ts
+++ src/app.config.ts
@@ -7,5 +7,25 @@ export default defineAppConfig({
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
-  }
+  },
+  tabBar: {
+    color: '#000000',
+    selectedColor: '#07c160',
+    backgroundColor: '#ffffff',
+    borderStyle: 'black',
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '首页',
+        iconPath: 'assets/tab-bar/home.png',
+        selectedIconPath: 'assets/tab-bar/home-active.png'
+      },
+      {
+        pagePath: 'pages/about/index',
+        text: '关于',
+        iconPath: 'assets/tab-bar/about.png',
+        selectedIconPath: 'assets/tab-bar/about-active.png'
+      }
+    ]
+  },
 })
